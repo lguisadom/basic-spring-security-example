17dffed9fbc46e8bfc731ad6e352dae0bcb5bb6a
Luis Guisado <lguisadom@gmail.com>
2023-01-30 21:57:51 -0500

    Customizando la seguridad de la api: administrador de credenciales de usuario y password,- con password no encodificada

--- src/main/java/com/lagm/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/lagm/springsecurity/config/WebSecurityConfig.java
@@ -1,2 +1,28 @@
-package com.lagm.springsecurity.config;public class WebSecurityConfig {
+package com.lagm.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        var user = User.withUsername("luis")
+                .password("password123")
+                .roles("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); // No apto para producción
+    }
 }
